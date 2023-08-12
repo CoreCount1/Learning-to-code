939b25da6518bc6ff8a176259159bfcc2be43f51
CoreCount1 <138834038+CoreCount1@users.noreply.github.com>
2023-08-11 19:42:19 -0600

    updated fizz buzz game
    i forgot the buzz part of the game somehow and fixed code to not look as ungly

--- fizz-buzz game.py
+++ fizz-buzz game.py
@@ -2,10 +2,12 @@ print("fizz-buzz game")
 print("due to poor technology (my brain) in canada i am not able to make a more interesting game")
 
 user_input_number = int(input("Put in a non decimal number"))
-if user_input_number % 3 == 0: # if divisible by 3 with 0 remainder
-    if user_input_number % 5 == 0: # if divisible by 5 with 0 remainder
-        print("fizz-buzz")
-    else: # if divisible by 3 but not 5
+if user_input_number % 3 == 0 and user_input_number % 5 == 0: # if divisible by 3 with 0 remainder and if divisible by 5 with 0 remainder
+    print("fizz-buzz")
+
+elif user_input_number % 5 == 0: # if divisible by 5 with 0 remainder
+    print("buzz")
+elif user_input_number % 3 == 0:  # if divisible by 3 but not 5
         print("fizz")
-else:
+else: 
     print(user_input_number)
