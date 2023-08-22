9efa1edf270802d583eaa1ee1a4d8bc0c552d1e3
CoreCount1 <138834038+CoreCount1@users.noreply.github.com>
2023-08-21 20:30:11 -0600

    Create fibonacci sequence generator.py

---
+++ fibonacci sequence generator.py
@@ -0,0 +1,3 @@
+print("strange fibonacci sequence generator")
+user_input_quanitity = int(input("how many numbers do you want to generate? "))
+user_input_starting_number = int(input("what number do you want to start with? "))
