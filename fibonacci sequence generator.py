e7371ca72fa63368d757a24b7ade648d7ec464d3
CoreCount1 <138834038+CoreCount1@users.noreply.github.com>
2023-08-22 17:50:33 -0600

    fibonacci sequence generator
    github co pilot FTW!!!!

--- fibonacci sequence generator.py
+++ fibonacci sequence generator.py
@@ -1,3 +1,18 @@
 print("strange fibonacci sequence generator")
+
 user_input_quanitity = int(input("how many numbers do you want to generate? "))
-user_input_starting_number = int(input("what number do you want to start with? "))
+first = 0
+second = 1
+i = 0
+
+while user_input_quanitity <= int(0):
+    print("if you dont wanna generate any numbers, why did you run this program?")
+    user_input_quanitity = int(input("how many numbers do you want to generate? "))
+
+# bold of you to assume i know how to do the next part without googling it
+
+while i < user_input_quanitity: # besides this, i did this part without googling it
+    print(first)
+    first = first + second
+    second = first - second
+    i += 1 # besides this, i did this part without googling it
